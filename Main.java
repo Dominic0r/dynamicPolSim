a47ab77917a39ed02391fde80eea1505ad5696dc
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-06-16 10:46:46 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -394,7 +394,7 @@ public class Main
                     relations.put(par, relations.get(par)- 20);
                 }
                 
-                if(this.getIdeology()>25 && this.getIdeology()<75){
+                if(this.getIdeology()>25||this.getIdeology()<75){
                     if(par.getIdeology()<25 && par.getIdeology()>75){
                         relations.put(par, relations.get(par)-25);
                     }
@@ -2464,7 +2464,7 @@ public static String[][] mapProgside = {
         allTotalGroups.add(new ideoGroup("Progressive Populists", "Progressive Peoples Party", 25, 55, 1909));
         allTotalGroups.add(new ideoGroup("Fascists", "Peoples National Revolutionary Party", 15, 3, 1915));
         allTotalGroups.add(new ideoGroup("Social Democrats", "Social Democratic Party", 20, 60, 1918));
-        allTotalGroups.add(new ideoGroup("Republican Conservatives", "Conservative Republican Party", 30, 38, 1918));
+        allTotalGroups.add(new ideoGroup("Republican Conservatives", "National Constitutional Party", 30, 38, 1918));
         allTotalGroups.add(new ideoGroup("Marxist Leninists", "Communist Revolutionary Party", 8, 90, 1923));
         allTotalGroups.add(new ideoGroup("Liberal Unionists", "Alliance of Republican Unions", 20, 53, 1927));
         
